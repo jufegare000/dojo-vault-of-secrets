366de3845a18f821b4a8613d5bfdb09831656e8c
juanfgallo <juanfgallo@localhost.localdomain>
2024-04-24 10:18:53 -0500

    first implementation

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-const main = () => {
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
+
+// Instantiates a client
+const client = new SecretManagerServiceClient();
+const main = async () => {
     console.log("holi")
+    await getSecret()
 }
 
+
+
+
+
+const getSecret = async () => {
+    const [secret] = await client.getSecret({
+        name: 'clean-cloud-secret',
+    });
+
+    console.info(`Found secret ${secret.name} `);
+}
 main();
