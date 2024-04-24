a21bfbaa63218f6fb079f688cff2a287c8cf5c77
juanfgallo <juanfgallo@localhost.localdomain>
2024-04-24 10:55:52 -0500

    obtained secret

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 
+
 // Instantiates a client
 const client = new SecretManagerServiceClient();
 const main = async () => {
@@ -7,15 +8,11 @@ const main = async () => {
     await getSecret()
 }
 
-
-
-
-
 const getSecret = async () => {
-    const [secret] = await client.getSecret({
-        name: 'clean-cloud-secret',
+    const [secret] = await client.accessSecretVersion({
+        name: 'projects/clean-cloud-415916/secrets/clean-cloud-secret/versions/latest'
     });
 
-    console.info(`Found secret ${secret.name} `);
+    console.info(`Found secret ${secret.payload.data.toString('utf8')} `);
 }
 main();
